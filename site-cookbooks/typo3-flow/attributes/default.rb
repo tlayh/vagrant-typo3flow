7e58e1ca8d4fed298e65195673abd7ec70e56cc4
Thomas Layh <thomas.layh@aoemedia.de>
2013-04-04 14:04:37 +0200

    orignal composer anyway?

--- site-cookbooks/typo3-flow/attributes/default.rb
+++ site-cookbooks/typo3-flow/attributes/default.rb
@@ -2,3 +2,5 @@ include_attribute "mysql::server"
 node.set[:mysql][:server_root_password] = 'root'
 node['mysql']['server_debian_password'] = 'root'
 node['mysql']['server_repl_password'] = 'root'
+
+node['composer']['install_globally'] = 'true'
